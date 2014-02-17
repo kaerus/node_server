5d3c7dd59a958c696149c2e4c7de9709a59b5f5d
Anders Elo <anders@kaerus.com>
2014-02-17 20:06:42 +0100

    handle accept, deflate or gzip

--- server.js
+++ server.js
@@ -1,45 +1,83 @@
 var fs = require('fs'),
     url = require('url'),
+    zlib = require('zlib'),
     http = require('http'), 
     logger = console;
 
+var CONFIG = {
+    port: 3210,
+    timeout: 60*1000,
+    path: ''
+}
 
-var SERVER_PORT = 3210, 
-    SERVER_TIMEOUT = 60*1000;
+process.argv.forEach(function(arg){
+    var option, value, i;
+
+    if(arg.indexOf('--') === 0){
+        if((i = arg.indexOf('=')) < 0 ) 
+            throw new Error("Invalid option without value", arg);
+
+        option = arg.substr(2,i-2);
+        value = arg.substr(i+1,arg.length);
+        
+        switch(option){
+            case 'port':
+                CONFIG.port = parseInt(value,10);
+                break;
+            case 'timeout':
+                CONFIG.timeout = parseInt(value,10);
+                break;
+            case 'path':
+                CONFIG.path = value;
+                break;
+        }
+    }
+})
+
+logger.info("Configuration: %j", CONFIG);
 
 function onRequest(req,res){
-    var path, data, type;
+    var path, data, type, encoding, compress = true;
 
-    logger.info("request", req.headers);
+    logger.info("Request %s %s from %s:%s, Headers", 
+        req.method, 
+        req.url, 
+        req.connection.remoteAddress,
+        req.connection.remotePort, 
+        req.headers);
 
     path = url.parse(req.url).pathname.replace(/^[\.\/]/,'');
 
-    path = path ? path : 'index.html';
+    path = CONFIG.path + (path ? path : 'index.html');
 
     if(path[path.length-1] ==='/') path+= 'index.html';
-    
-    logger.info("request url %s ->", req.url, path);
     
     req.on('error',function(err){
         console.log("req error", err);
     });
 
-    if((type = path.match(/\.(.*)$/))) {
-        switch(type[1]){
-            case 'html':
+    if((type = path.match(/(\.[\w]+)$/))) {
+        type = type[0].toLowerCase();
+        switch(type){
+            case '.html':
+            case '.htm':
                 type = "text/html";
                 break;
-            case 'js':
+            case '.js':
                 type = "text/javascript";
                 break;
-            case 'png':
+            case '.png':
                 type = "image/png";
                 break;
-            case 'jpeg':
-            case 'jpg':
-                type ="image/jpg"
+            case '.jpeg':
+            case '.jpg':
+                type ="image/jpg";
+                compress = false;
                 break;
-            case 'css':
+            case '.svg':
+                type = "image/svg+xml";
+                break;
+            case '.css':
                 type = "text/css";
                 break;         
             default:
@@ -52,23 +90,40 @@ function onRequest(req,res){
         res.writeHead(404,"Unkown file type");
         console.log("unknown type for path", path);
     } else {
+        
+        if (!(encoding = req.headers['accept-encoding'])){
+            encoding = '';
+        }
 
         try {
-            data = fs.readFileSync(path);
+            data = fs.createReadStream(path);
         } catch(err){
             res.writeHead(404, err);
             res.end();
             console.log("Failed to read", path);
             return;
         }
-        console.log("serving %s data from", type, path);
-        res.writeHead(200,{"content-type": type});
-        res.end(data);   
+
+        if(compress && encoding.match(/\bdeflate\b/)) {
+            encoding = 'deflate';
+            res.writeHead(200, {'content-encoding':type,'content-encoding': encoding});
+            data.pipe(zlib.createDeflate()).pipe(res);
+        } else if(compress && encoding.match(/\bgzip\b/)) {
+            encoding = 'gzip';
+            res.writeHead(200, {'content-encoding':type, 'content-encoding': encoding });
+            data.pipe(zlib.createGzip()).pipe(res);
+        } else {
+            encoding = '';
+            res.writeHead(200, {"content-type": type});
+            data.pipe(res);
+        }
+
+        logger.info("Response", path, type, encoding);
     }
 }
 
 function onServer() {
-    logger.info("Server start", this.address());
+    logger.info("Server listen", this.address());
 }
 
 
@@ -89,9 +144,9 @@ function onConnection(socket){
         logger.info("client close", client);
     });
 
-    socket.setTimeout(SERVER_TIMEOUT);
+    socket.setTimeout(CONFIG.timeout);
 }
 
 http.createServer(onRequest)
-    .listen(SERVER_PORT, onServer)
+    .listen(CONFIG.port, onServer)
     .on('connection',onConnection);
